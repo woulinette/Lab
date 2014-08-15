6d901929f729c03553b98a1a13684a810834f318
Aurélie FOUCHER <aureliefoucher@me.com>
2014-08-15 18:44:51 +0200

    Node modules installation

---
+++ config.rb
@@ -0,0 +1,5 @@
+require "susy"
+css_dir = 'portfolio/public/css'
+sass_dir = 'portfolio/sources/sass'
+javascript_dir = 'portfolio/public/js'
+output_style = :compressed
---
+++ gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.initConfig({
+		uglify: {
+			my_target: {
+				files: {
+					'portfolio/public/js/script.js' : ['portfolio/sources/js/*.js']
+				} //files
+			} //my_target
+		}, //uglify
+		compass: {
+			dev: {
+				options: {
+					config: 'config.rb'
+				} //options
+			} //dev
+		}, //compass
+		watch: {
+			options: { livereload: true },
+			scripts: {
+			files: 	 ['portfolio/sources/js/*.js'],
+			tasks:   ['uglify']	
+			}, //script
+			sass: {
+				files: ['portfolio/sources/sass/*.scss'],
+				tasks: ['compass:dev']
+			}, //sass
+			html: {
+				files: ['portfolio/*.html']
+			}
+		} //watch
+	}) //initConfig	
+	grunt.registerTask('default', 'watch')
+} //exports
